bc3ab11128fb2d6aa1ec25cf1ce781e348fac1da
rudi gresik <rudi2783@gmail.com>
2020-08-12 13:01:18 +0700

    add aplication Master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Header } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
+
+import {
+ BrowserRouter as Router,
+ Switch,
+ Route,
+} from "react-router-dom";
+import Home from './Home';
+import Register from './Register';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename='/web'>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path='/Register'>
+          <Register/>
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+ }
+ 
 
 export default App;
